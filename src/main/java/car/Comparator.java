4ca94543e6e501a8c9eae88fea746d6281b744d4
vadyusha-grigorev-2002 <vadyusha-grigorev-2002@mail.ru>
2024-10-30 15:50:54 +0300

    New Branch

---
+++ src/main/java/car/Comparator.java
@@ -0,0 +1,23 @@
+package car;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Comparator{
+    public static void main(String[] args)
+    {
+        ArrayList<String> list = new ArrayList<String>();
+        Collections.addAll(list, "Привет", "как", "дела?");
+        Collections.sort(list);
+    }
+}
+
+class StringLengthComparator implements Comparator<String>
+{
+    public int compare (String obj1, String obj2)
+    {
+        return obj1.length() - obj2.length();
+    }
+}
+
+
